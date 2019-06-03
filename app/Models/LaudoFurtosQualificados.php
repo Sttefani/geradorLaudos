ef39bd0894061c2f1779442f34d482c05dcbb86f
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-06-02 21:49:51 -0300

    criando LaudoFurtosQualificados.php

---
+++ app/Models/LaudoFurtosQualificados.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LaudoFurtosQualificados extends Model
+{
+    protected $fillable = [
+        'user_id', 'numero_do_laudo', 'data_da_ocorrencia', 'data_hora_do_acionamento', 'data_hora_da_chegada_ao_local',
+        'perito', 'diretor', 'numero_da_requisicao', 'numero_do_procedimento', 'origem_da_requisicao',
+        'autoridade_requisitante', 'utilizacao', 'recuo_frontal', 'recuo_lateral_direita', 'recuo_lateral_esquerda',
+        'recuo_fundos', 'area_do_imovel', 'area_construida', 'numero_de_pavimentos', 'numero_de_compartimentos',
+        'tipo_de_construcao', 'piso', 'cobertura', 'forro', 'esquadrias', 'classificacao', 'estado_de_conservacao',
+        'papiloscopia_no_local', 'local_violado', 'proprietario_posseiro', 'endereco', 'cidade',
+        'exames', 'conclusao', 'foto_1', 'comentario_1', 'foto_2', 'comentario_2', 'foto_3', 'comentario_3',
+        'foto_4', 'comentario_4', 'foto_5', 'comentarios_5', 'foto_6', 'comentarios_6'
+    ];
+
+    
+}
